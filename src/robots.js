75afa90f7ce89e36ab732e3f2498b89f6f88e29f
skyrippa <iagob4599@gmail.com>
2019-03-21 23:21:39 -0300

    adding project files

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Luffy-taro',
+		username: 'Monkey',
+		email: 'king@goa.com'
+	},
+	{
+		id: 2,
+		name: 'Zoro',
+		username: 'Roronoa',
+		email: 'espada@wano.com'
+	},
+	{
+		id: 3,
+		name: 'Nami',
+		username: 'Ninja',
+		email: 'gata@coco.com'
+	},
+	{
+		id: 4,
+		name: 'Usopp',
+		username: 'Uso',
+		email: 'cap@eastblue.com'
+	},
+	{
+		id: 5,
+		name: 'Sangoro',
+		username: 'Vinsmoke',
+		email: 'cook@northblue.com'
+	},
+	{
+		id: 6,
+		name: 'Robin',
+		username: 'Nico',
+		email: 'smart@history.com'
+	},
+	{
+		id: 7,
+		name: 'Franosuke',
+		username: 'Super',
+		email: 'super@water7.com'
+	},
+	{
+		id: 8,
+		name: 'Brook',
+		username: 'Soul King',
+		email: 'bone@bone.com'
+	},
+	{
+		id: 9,
+		name: 'Chopper',
+		username: 'Tony-Tony',
+		email: 'doc@drum.com'
+	},
+	{
+		id: 10,
+		name: 'Jimbe',
+		username: 'Sea Boy',
+		email: 'seaboy@tritao.com'
+	},
+]
