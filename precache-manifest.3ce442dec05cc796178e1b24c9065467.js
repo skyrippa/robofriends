69036049023d137ca73f0d51008bad418123655d
skyrippa <iagob4599@gmail.com>
2019-05-30 21:58:39 -0300

    Updates

---
+++ precache-manifest.3ce442dec05cc796178e1b24c9065467.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aec4dcc16065c0473446",
+    "url": "/robofriends/static/css/main.8a2e01b8.chunk.css"
+  },
+  {
+    "revision": "aec4dcc16065c0473446",
+    "url": "/robofriends/static/js/main.d9d47643.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "fff3bf9b0ea357ecec86",
+    "url": "/robofriends/static/css/2.34f10b97.chunk.css"
+  },
+  {
+    "revision": "fff3bf9b0ea357ecec86",
+    "url": "/robofriends/static/js/2.bf19b1f6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "914e78c7fc9f2af46e6acd042e2fe109",
+    "url": "/robofriends/index.html"
+  }
+];
